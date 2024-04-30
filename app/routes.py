58f68b4cecd4600c985f845e186efd8fe5262b7f
ZnakeDenmark <Victor.dyhrman@gmail.com>
2024-04-30 09:11:21 +0200

    Tilføjet nolge få ting

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,14 @@
 from flask import render_template, flash, redirect
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, Items
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    form = Items()
+    user = {'username': 'wah wah'}
+
+    return render_template('index.html', title='Home', user=user, form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -26,3 +18,8 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/kurv', methods=['GET', 'POST'])
+def kurv():
+    form = LoginForm()
+    return render_template('kurv.html', title='Kurv', form=form)
