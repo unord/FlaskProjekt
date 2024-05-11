1f9d675541a33364ca3479e097264aedb6110cda
Victor Dyhrman <victor.dyhrman@gmail.com>
2024-05-11 15:24:49 +0200

    Tilføjet login og database dertil

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,58 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for, request
 from app import app
-from app.forms import LoginForm, Items
+from app.forms import LoginForm, Items, RegistrationForm
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
+from flask_login import logout_user, login_required
+from urllib.parse import urlsplit
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     form = Items()
-    user = {'username': 'wah wah'}
-
-    return render_template('index.html', title='Home', user=user, form=form)
+    user="whatever"
+    return render_template("index.html", title='Home Page', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/kurv', methods=['GET', 'POST'])
 def kurv():
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jakse\OneDrive\Dokumenter\GitHub\MLKaggle.min\FlaskProjekt\venv
+set VIRTUAL_ENV=C:\Users\victo\Documents\FlaskProjekt\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
